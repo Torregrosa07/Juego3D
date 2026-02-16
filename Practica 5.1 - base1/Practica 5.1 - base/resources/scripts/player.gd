eaa593930178f17919dfb33f233632acf4cd2dd6
Santiago Torregrosa <santiago.torre07a@gmail.com>
2026-02-16 22:11:51 +0100

    cambios

--- Practica 5.1 - base1/Practica 5.1 - base/resources/scripts/player.gd
+++ Practica 5.1 - base1/Practica 5.1 - base/resources/scripts/player.gd
@@ -6,20 +6,37 @@ var axis: Vector2
 var rot: Vector3
 var angle: float
 
+#Variables para el doble salto
+var jump_count: int = 0
+const MAX_JUMPS: int = 1
+
 const SPEED = 12
 const GRAVITY = 2
 const JUMP = 30
 
 func _process(_delta):
+	# Si estamos en el suelo, reiniciamos el contador de saltos
+	if is_on_floor():
+		jump_count = 0
+		
 	match can_move:
 		true:
 			motion_ctrl()
 
 func _input(event):
-	if is_on_floor() and event.is_action_pressed("ui_accept"):
-		velocity.y = JUMP
-		$AudioJump.play()
-		
+	#Lógica del salto
+	if event.is_action_pressed("ui_accept"):
+		# Saltamos si estamos en el suelo O si llevamos menos saltos que el máximo
+		if is_on_floor() or jump_count < MAX_JUMPS:
+			velocity.y = JUMP
+			jump_count += 1 # Sumamos un salto al contador
+			$AudioJump.play()
+			
+			#Si es el segundo salto y quieres reiniciar la animación para que se note
+			if jump_count > 1:
+				$AnimationPlayer.stop()
+				$AnimationPlayer.play("Jump")
+
 func get_axis():
 	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
